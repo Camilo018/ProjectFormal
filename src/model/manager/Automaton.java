f11df72fdd46682c973c77c895b5eada3fd4e9b9
brayan pineda <brayansteven67@gmail.com>
2021-05-15 21:12:31 -0500

    voy en el automata

--- src/model/manager/Automaton.java
+++ src/model/manager/Automaton.java
@@ -13,4 +13,12 @@ public class Automaton {
         this.formalism = new Formalism();
     }
 
+    public void addState (Status status) {
+        automatonGraph.add(status);
+    }
+
+    public void addTransitions (Status originStatus, String symbol,Status destinationStatus) throws IndexOutOfBoundsException{
+        automatonGraph.addAssociation(originStatus,destinationStatus,symbol);
+    }
+
 }
